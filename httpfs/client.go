32ff7cb2ce3efc3e248c3017becab43437179006
quieoo <quieoob@gmail.com>
2022-04-11 16:00:49 +0800

    httpfs, trace upload and download

--- httpfs/client.go
+++ httpfs/client.go
@@ -3,21 +3,23 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/rand"
 	"flag"
 	"fmt"
 	"github.com/rcrowley/go-metrics"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
 const MS = 1000000
+const downloadfilepath = "./downloaded"
 
 func main() {
 	var cmd string
@@ -26,160 +28,43 @@ func main() {
 	var concurrentGet int
 	var filenames string
 	var host string
-	const downloadfilepath = "downloaded"
+	var ips []string
+	var trace_workload string
+	var servers int
+	var traceDownload_randomRequest bool
 
 	flag.StringVar(&cmd, "c", "", "operation type\n"+
 		"upload: upload file with -s for file size, -n for file number, -fn for specified name of file storing all uploaded file name\n"+
-		"download: download files following specified filename\n")
-	flag.IntVar(&filesize, "s", 256*1024, "file size")
+		"download: download files following specified filename\n"+
+		"traceDownload: download according to generated trace file\n")
+
+	flag.IntVar(&filesize, "size", 256*1024, "file size")
 	flag.IntVar(&filenumber, "n", 1, "file number")
 	flag.IntVar(&concurrentGet, "cg", 1, "concurrent get number")
 	flag.StringVar(&filenames, "fn", "filenames", "name of files for uploading")
-	flag.StringVar(&host, "h", "127.0.0.1", "server address")
 
+	flag.StringVar(&host, "h", "127.0.0.1", "server addresses, seperated by commas")
+	flag.BoolVar(&traceDownload_randomRequest, "randomRequest", false, "random request means that current client will randomly reorder requests from generated workload")
+	flag.StringVar(&trace_workload, "f", "", "file indicates the path of workload file of generated trace")
+	flag.IntVar(&servers, "s", 1, "servers indicates the total number of servers")
 	flag.Parse()
 
 	uploadTimer := metrics.NewTimer()
 	downloadTimer := metrics.NewTimer()
 
-	if cmd == "upload" {
-		fn, _ := os.Create(filenames)
-		fmt.Printf("Uploading files with size %d B\n", filesize)
-
-		createDirIfNotExist("./temp")
-
-		for i := 0; i < filenumber; i++ {
-			//create temporary file
-			name := fmt.Sprintf("%d-%d", filesize, i)
-			subs := NewLenChars(filesize, StdChars)
-			err := ioutil.WriteFile("./temp/"+name, []byte(subs), 0666)
-			if err != nil {
-				fmt.Println("failed to write temporary file: ", err.Error())
-				return
-			}
-
-			uploadstart := time.Now()
-			//upload file
-			bodyBuffer := &bytes.Buffer{}
-			bodyWriter := multipart.NewWriter(bodyBuffer)
-
-			fileWriter, _ := bodyWriter.CreateFormFile("files", name)
-
-			file, _ := os.Open("./temp/" + name)
-
-			_, err = io.Copy(fileWriter, file)
-			if err != nil {
-				fmt.Println("failed to copy file to buffer: ", err.Error())
-				return
-			}
-
-			contentType := bodyWriter.FormDataContentType()
-			bodyWriter.Close()
-
-			resp, _ := http.Post("http://"+host+":8080/upload", contentType, bodyBuffer)
-
-			resp.Body.Close()
-			file.Close()
-			//resp_body, _ := ioutil.ReadAll(resp.Body)
-			uploadTimer.Update(time.Now().Sub(uploadstart))
-			//name -> filename
-			_, err = io.WriteString(fn, name+"\n")
-			if err != nil {
-				fmt.Println("failed to store filename: ", err.Error())
-				return
-			}
-		}
-
-		//cleaning temporary files
-		_, err := os.Stat("./temp")
-		if !(err != nil && os.IsNotExist(err)) {
-			err := os.RemoveAll("./temp")
-			if err != nil {
-				fmt.Println("failed to remove temp directory: ", err.Error())
-				return
-			}
-		}
-
-		//output metrics
-		fmt.Printf(standardOutput("http-upload", uploadTimer))
-	} else if cmd == "download" {
-		file, err := os.Open(filenames)
-		defer file.Close()
-		if err != nil {
-			fmt.Println("failed to open filenames to read file name: ", err.Error())
-			return
-		}
-		//br := bufio.NewReader(file)
-
-		createDirIfNotExist(downloadfilepath)
-
-		// 把cid读到多个切片中
-		inputReader := bufio.NewReader(file)
-
-		allFileName := make([][]string, concurrentGet)
-		for i := range allFileName {
-			allFileName[i] = make([]string, 0)
-		}
-
-		tmpCnt := 0
-		for {
-			aLine, readErr := inputReader.ReadString('\n')
-			aLine = strings.TrimSuffix(aLine, "\n")
-			if readErr == io.EOF {
-				break
-			}
-			allFileName[tmpCnt%concurrentGet] = append(allFileName[tmpCnt%concurrentGet], aLine)
-			tmpCnt++
-		}
-
-		// 创建多个协程，分别去get文件
-		var wg sync.WaitGroup
-		wg.Add(concurrentGet)
-		for i := 0; i < concurrentGet; i++ {
-			go func(theOrder int) {
-				defer wg.Done()
-				for j := 0; j < len(allFileName[theOrder]); j++ {
-					toRequest := allFileName[theOrder][j]
-
-					downloadstrat := time.Now()
-					url := "http://" + host + ":8080/files/" + toRequest
-					res, err := http.Get(url)
-					if err != nil {
-						fmt.Println("failed to request file: ", err.Error())
-						return
-					}
-
-					f, err := os.Create(downloadfilepath + "/" + toRequest)
-					if err != nil {
-						fmt.Println("failed to create local file: ", err.Error())
-						return
-					}
-					_, err = io.Copy(f, res.Body)
-					if err != nil {
-						fmt.Println("failed to copy response to local file: ", err.Error())
-						return
-					}
-					downloadTimer.Update(time.Now().Sub(downloadstrat))
-
-					if err != nil {
-						fmt.Printf("thread %d downloading stall caused by %s", theOrder, err.Error())
-						fmt.Printf(standardOutput("http-download", downloadTimer))
-						return
-					} else if j == len(allFileName[theOrder])-1 {
-						fmt.Printf("thread %d downloading stall caused by havingfilesystem:https://docs.google.com/persistent/docs/documents/1HT8zjCAQULYnfpc_EahYXJ0MRoEIW53cR_zucqMB7wU/image/PLACEHOLDER_1e1273deb9e66546_0 downloaded all needed files.\n", theOrder)
-						fmt.Printf(standardOutput("http-download", downloadTimer))
-						return
-					}
-				}
-
-			}(i)
-		}
-		wg.Wait()
-
-	} else if cmd == "tracUpload" {
-		traceFile := ""
+	ips = strings.Split(host, ",")
+	if len(ips) != servers {
+		fmt.Printf("the number of given ips %d is not equal with server number %d\n", len(ips), servers)
+		return
 	}
 
+	if cmd == "upload" {
+		Upload(filenames, filesize, filenumber, ips[0], uploadTimer)
+	} else if cmd == "download" {
+		Download(filenames, downloadfilepath, concurrentGet, ips[0], downloadTimer)
+	} else if cmd == "traceDownload" {
+		TraceDownload(trace_workload, traceDownload_randomRequest, ips, servers, downloadTimer)
+	}
 }
 
 var StdChars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
@@ -233,4 +118,212 @@ func createDirIfNotExist(path string) {
 
 func standardOutput(function string, t metrics.Timer) string {
 	return fmt.Sprintf("%s: %d files, average latency: %f ms, 0.99P latency: %f ms\n", function, t.Count(), t.Mean()/MS, t.Percentile(float64(t.Count())*0.99)/MS)
+}
+
+func Upload(filenames string, filesize int, filenumber int, host string, uploadTimer metrics.Timer) {
+	fn, _ := os.Create(filenames)
+	fmt.Printf("Uploading files with size %d B\n", filesize)
+
+	createDirIfNotExist("./temp")
+
+	for i := 0; i < filenumber; i++ {
+		//create temporary file
+		name := fmt.Sprintf("%d-%d", filesize, i)
+		subs := NewLenChars(filesize, StdChars)
+		err := ioutil.WriteFile("./temp/"+name, []byte(subs), 0666)
+		if err != nil {
+			fmt.Println("failed to write temporary file: ", err.Error())
+			return
+		}
+
+		uploadstart := time.Now()
+		//upload file
+		bodyBuffer := &bytes.Buffer{}
+		bodyWriter := multipart.NewWriter(bodyBuffer)
+
+		fileWriter, _ := bodyWriter.CreateFormFile("files", name)
+
+		file, _ := os.Open("./temp/" + name)
+
+		_, err = io.Copy(fileWriter, file)
+		if err != nil {
+			fmt.Println("failed to copy file to buffer: ", err.Error())
+			return
+		}
+
+		contentType := bodyWriter.FormDataContentType()
+		bodyWriter.Close()
+
+		resp, _ := http.Post("http://"+host+":8080/upload", contentType, bodyBuffer)
+
+		resp.Body.Close()
+		file.Close()
+		//resp_body, _ := ioutil.ReadAll(resp.Body)
+		uploadTimer.Update(time.Now().Sub(uploadstart))
+		//name -> filename
+		_, err = io.WriteString(fn, name+"\n")
+		if err != nil {
+			fmt.Println("failed to store filename: ", err.Error())
+			return
+		}
+	}
+
+	//cleaning temporary files
+	_, err := os.Stat("./temp")
+	if !(err != nil && os.IsNotExist(err)) {
+		err := os.RemoveAll("./temp")
+		if err != nil {
+			fmt.Println("failed to remove temp directory: ", err.Error())
+			return
+		}
+	}
+
+	//output metrics
+	fmt.Printf(standardOutput("http-upload", uploadTimer))
+}
+
+func Download(filenames string, downloadfilepath string, concurrentGet int, host string, downloadTimer metrics.Timer) {
+	file, err := os.Open(filenames)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("failed to open filenames to read file name: ", err.Error())
+		return
+	}
+	//br := bufio.NewReader(file)
+
+	createDirIfNotExist(downloadfilepath)
+
+	// 把cid读到多个切片中
+	inputReader := bufio.NewReader(file)
+
+	allFileName := make([][]string, concurrentGet)
+	for i := range allFileName {
+		allFileName[i] = make([]string, 0)
+	}
+
+	tmpCnt := 0
+	for {
+		aLine, readErr := inputReader.ReadString('\n')
+		aLine = strings.TrimSuffix(aLine, "\n")
+		if readErr == io.EOF {
+			break
+		}
+		allFileName[tmpCnt%concurrentGet] = append(allFileName[tmpCnt%concurrentGet], aLine)
+		tmpCnt++
+	}
+
+	// 创建多个协程，分别去get文件
+	var wg sync.WaitGroup
+	wg.Add(concurrentGet)
+	for i := 0; i < concurrentGet; i++ {
+		go func(theOrder int) {
+			defer wg.Done()
+			for j := 0; j < len(allFileName[theOrder]); j++ {
+				toRequest := allFileName[theOrder][j]
+
+				downloadstrat := time.Now()
+				url := "http://" + host + ":8080/files/" + toRequest
+				res, err := http.Get(url)
+				if err != nil {
+					fmt.Println("failed to request file: ", err.Error())
+					return
+				}
+
+				f, err := os.Create(downloadfilepath + "/" + toRequest)
+				if err != nil {
+					fmt.Println("failed to create local file: ", err.Error())
+					return
+				}
+				_, err = io.Copy(f, res.Body)
+				if err != nil {
+					fmt.Println("failed to copy response to local file: ", err.Error())
+					return
+				}
+				downloadTimer.Update(time.Now().Sub(downloadstrat))
+
+				if err != nil {
+					fmt.Printf("thread %d downloading stall caused by %s", theOrder, err.Error())
+					fmt.Printf(standardOutput("http-download", downloadTimer))
+					return
+				} else if j == len(allFileName[theOrder])-1 {
+					fmt.Printf("thread %d downloading stall caused by havingfilesystem:https://docs.google.com/persistent/docs/documents/1HT8zjCAQULYnfpc_EahYXJ0MRoEIW53cR_zucqMB7wU/image/PLACEHOLDER_1e1273deb9e66546_0 downloaded all needed files.\n", theOrder)
+					fmt.Printf(standardOutput("http-download", downloadTimer))
+					return
+				}
+			}
+
+		}(i)
+	}
+	wg.Wait()
+}
+
+func TraceDownload(trace_workload string, traceDownload_randomRequest bool, ips []string, servers int, downloadTimer metrics.Timer) {
+	// read trace workload file
+	tracefile := trace_workload
+	if traces, err := os.Open(tracefile); err != nil {
+		fmt.Printf("failed to open trace file: %s, due to %s\n", tracefile, err.Error())
+		return
+	} else {
+		if _, err := os.Stat(downloadfilepath); os.IsNotExist(err) {
+			os.Mkdir(downloadfilepath, 0777)
+			//os.Chmod(downloadfilepath, 0777)
+		}
+
+		defer traces.Close()
+		scanner := bufio.NewScanner(traces)
+		var names []int
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			codes := strings.Split(line, "\t")
+			name, _ := strconv.Atoi(codes[1])
+			names = append(names, name)
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Cannot scanner text file: %s, err: [%v]\n", tracefile, err)
+			return
+		}
+
+		//randomize request order
+		if traceDownload_randomRequest {
+			fmt.Println("randomizing request queue")
+			rand.Seed(time.Now().Unix())
+			rand.Shuffle(len(names), func(i, j int) {
+				names[i], names[j] = names[j], names[i]
+			})
+		}
+
+		n := len(names)
+		//request to each server
+		for i := 0; i < n; i++ {
+			if i%100 == 0 {
+				fmt.Printf("downloading %.2f \n", float64(i)/float64(n)*100)
+			}
+			toRequest := names[i]
+			hs := ips[toRequest%servers]
+			requestFile := strconv.Itoa(toRequest)
+
+			downloadstrat := time.Now()
+			url := "http://" + hs + ":8080/files/" + requestFile
+			res, err := http.Get(url)
+			if err != nil {
+				fmt.Println("failed to request file: ", err.Error())
+				return
+			}
+
+			f, err := os.Create(downloadfilepath + "/" + requestFile)
+			if err != nil {
+				fmt.Println("failed to create local file: ", err.Error())
+				return
+			}
+			_, err = io.Copy(f, res.Body)
+			if err != nil {
+				fmt.Println("failed to copy response to local file: ", err.Error())
+				return
+			}
+			f.Close()
+			downloadTimer.Update(time.Now().Sub(downloadstrat))
+		}
+		fmt.Println(standardOutput("trace-download", downloadTimer))
+	}
 }
--- httpfs/server.go
+++ httpfs/server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const filePath = "files"
@@ -42,6 +47,39 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var STD = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+// NewLenChars returns a new random string of the provided length, consisting of the provided byte slice of allowed characters(maximum 256).
+func RanDomContent(length int, chars []byte) string {
+	if length == 0 {
+		return ""
+	}
+	clen := len(chars)
+	if clen < 2 || clen > 256 {
+		panic("Wrong charset length for NewLenChars()")
+	}
+	maxrb := 255 - (256 % clen)
+	b := make([]byte, length)
+	r := make([]byte, length+(length/4)) // storage for random bytes.
+	i := 0
+	for {
+		if _, err := rand.Read(r); err != nil {
+			panic("Error reading random bytes: " + err.Error())
+		}
+		for _, rb := range r {
+			c := int(rb)
+			if c > maxrb {
+				continue // Skip this number to avoid modulo bias.
+			}
+			b[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(b)
+			}
+		}
+	}
+}
+
 func main() {
 
 	_, err := os.Stat(filePath)
@@ -58,6 +96,65 @@ func main() {
 		}
 	}
 
+	var cmd string
+	var index int
+	var servers int
+	var trace_docs string
+	flag.StringVar(&cmd, "c", "", "operation type\n"+
+		"traceUpload: generate trace files\n")
+	flag.StringVar(&trace_docs, "f", "", "file indicates the path of doc file of generated trace")
+	flag.IntVar(&index, "i", 0, "given each server has a part of entire workload, index indicates current server own the i-th part of data. Index starts from 0.")
+	flag.IntVar(&servers, "s", 1, "servers indicates the total number of servers")
+	flag.Parse()
+
+	if cmd == "traceUpload" {
+		if index < 0 || index >= servers {
+			fmt.Println("Error, the index is out of range")
+			return
+		}
+		tracefile := trace_docs
+		if f, err := os.Open(tracefile); err != nil {
+			fmt.Printf("failed to open trace file: %s, due to %s\n", tracefile, err.Error())
+			return
+		} else {
+			defer f.Close()
+			//parse and generate related file, with trace format:
+			// ItemID | Popularity | Size(Bytes) | Application Type
+			scanner := bufio.NewScanner(f)
+			var sizes []int
+
+			for scanner.Scan() {
+				line := scanner.Text()
+				codes := strings.Split(line, "\t")
+				size, _ := strconv.Atoi(codes[2])
+				sizes = append(sizes, size)
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Cannot scanner text file: %s, err: [%v]\n", tracefile, err)
+				return
+			}
+
+			//determine the server provide what files
+			fileNumbers := len(sizes)
+			for i := 0; i < fileNumbers; i++ {
+				if i%100 == 0 {
+					fmt.Printf("uploading %d\n", i)
+				}
+				if i%servers == index {
+
+					name := strconv.Itoa(i)
+					size := sizes[i]
+					content := RanDomContent(size, STD)
+					err := ioutil.WriteFile("./"+filePath+"/"+name, []byte(content), 0666)
+					if err != nil {
+						fmt.Println("failed to generate file: ", err.Error())
+						return
+					}
+				}
+			}
+
+		}
+	}
 	http.HandleFunc("/upload", uploadHandler)
 
 	fs := http.FileServer(http.Dir(filePath))
