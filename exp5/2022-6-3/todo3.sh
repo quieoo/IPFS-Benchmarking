4db10fc47d48efffa7c7f786440eef36260be8a9
mazhaojia123 <55593798+mazhaojia123@users.noreply.github.com>
2022-06-06 11:06:54 +0800

    add PeerResponseHistory test doc and related files

---
+++ exp5/2022-6-3/todo3.sh
@@ -0,0 +1,24 @@
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.95 -rmn nb -loadsavecache > acn-0_95.out ;
+
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.96 -rmn nb -loadsavecache > acn-0_96.out ;
+
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.97 -rmn nb -loadsavecache > acn-0_97_1.out ;
+
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.98 -rmn nb -loadsavecache > acn-0_98.out ;
+
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.95 -rmn nb -loadsavecache > acn-0_95_1.out ;
+
+rm ../.ipfs -rf; ./../ipfs init; cp cache.txt.converge cache.txt;
+
+./localIPFSNode -c downloads -cid acid-new -enablemetrics -PeerRH -B 0.99 -rmn nb -loadsavecache > acn-0_99.out ;
+
